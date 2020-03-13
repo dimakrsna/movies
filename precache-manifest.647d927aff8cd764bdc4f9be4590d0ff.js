742181dfa99f0848839f9c1641e09bfc82fbbc2d
dimakrsna <dimakrsna@gmail.com>
2020-03-13 20:38:45 +0200

    Updates

---
+++ precache-manifest.647d927aff8cd764bdc4f9be4590d0ff.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "04129ba559ae92aa086e",
+    "url": "/movies/static/js/runtime~main.712e9881.js"
+  },
+  {
+    "revision": "2939022b48aa61ce98d1",
+    "url": "/movies/static/js/main.3b688b31.chunk.js"
+  },
+  {
+    "revision": "f7b62c76e16a85b40174",
+    "url": "/movies/static/js/2.bbff1c93.chunk.js"
+  },
+  {
+    "revision": "14398b052a09f90b50992da0d15ebe8b",
+    "url": "/movies/index.html"
+  }
+];
